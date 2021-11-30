a2bb6fcfc0103efd555dc35566988007db576913
Devin Bowman <devinbowman99@gmail.com>
2021-11-29 17:32:01 -0700

    Please don't have node modules screw me

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,12 +16,90 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-const mongoose = require('mongoose');
 
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  path: String,
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
+
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  Item.deleteMany({});
+  const item = new Item({
+    title: req.body.title,
+    path: req.body.path,
+    description: req.body.description,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 // connect to the database
 mongoose.connect('mongodb://localhost:27017/museum', {
   useNewUrlParser: true
 });
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    var current_item = await Item.findOne({
+      _id: req.params.id
+    //  _title: req.params.title
+    });
+    current_item.title = req.body.title;
+    current_item.description = req.body.description;
+    current_item.save();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
